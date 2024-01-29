a485ad2a1e1c35406037feeb9600d1c0902925e0
BoomBoom135 <157538526+BoomBoom135@users.noreply.github.com>
2024-01-29 12:50:59 -0500

    Add files via upload

---
+++ CP 164/sayy3423_l04/src/t07.py
@@ -0,0 +1,16 @@
+"""
+-------------------------------------------------------
+t07
+-------------------------------------------------------
+Author:  Nicholas Sayyad Shahbaz
+ID:      169063423
+Email:   sayy3423@mylaurier.ca
+__updated__ = "2024-01-26"
+-------------------------------------------------------
+"""
+# Imports
+from utilities import list_test
+from List_array import List
+
+source = []
+list_test(source)
