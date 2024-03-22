15c35ba96da4866a0afc215389527c9e75bd7643
BoomBoom135 <157538526+BoomBoom135@users.noreply.github.com>
2024-03-22 12:35:10 -0400

    Add files via upload

---
+++ CP 164/sayy3423_l10/src/t01.py
@@ -0,0 +1,12 @@
+"""
+-------------------------------------------------------
+t01
+-------------------------------------------------------
+Author:  Nicholas Sayyad Shahbaz
+ID:      169063423
+Email:   sayy3423@mylaurier.ca
+__updated__ = "2024-03-22"
+-------------------------------------------------------
+"""
+# Imports
+from test_Sorts_array import create_sorted, create_reversed, create_randoms
